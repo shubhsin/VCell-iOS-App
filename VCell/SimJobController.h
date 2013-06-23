77577f62446c3e29898f8e6c6fb30fa42f7fe4f7
Ankit Aggarwal <ankit.spd@gmail.com>
2013-06-23 21:39:52 +0530

    test commit

--- VCell/SimJobController.h
+++ VCell/SimJobController.h
@@ -28,8 +28,8 @@
 #define BIOMODEL_SCOPE 3
 
 @interface SimJobController : UITableViewController <MBProgressHUDDelegate,UISearchBarDelegate, UISearchDisplayDelegate>
-- (IBAction)addMoreCells:(id)sender;
 
+- (IBAction)addMoreCells:(id)sender;
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 - (IBAction)bioModelDateSwap:(id)sender;
 - (void)updatDataOnBtnPressedWithButtonTag:(int)tag AndButtonActive:(BOOL)active;
